ef321c6da526f1df4be62d1fabf4c46fd5fb25e8
Luke A <alvarengalucas55@gmail.com>
2019-01-28 20:22:18 -0200

    Created about page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import redirect, url_for
+from flask import render_template, redirect, url_for
 from app import app
 
 @app.route("/")
 @app.route("/index/")
 def index():
     return redirect(url_for("auth.login"))
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
